a1feef567a2d052df75c1a2173dc68c209463d36
entslscheia <yugu772@yahoo.com>
2017-03-30 15:50:59 +0800

    tree structure

--- Concept.java
+++ Concept.java
@@ -1,6 +1,16 @@
 package name.zanbry.cdc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Concept {
-	private int id;
-	private String uri;
+	public int id;
+	public Concept upperClass;
+	public List<Concept> lowerClasses;
+	
+	public Concept(int id){
+		this.id = id;
+		upperClass = null;
+		lowerClasses = new ArrayList<Concept>();
+	}
 }
--- Ontology.java
+++ Ontology.java
@@ -1,5 +1,24 @@
 package name.zanbry.cdc;
 
-public class Ontology {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ontology {
+	public Concept root;
+	public List<Concept> allConcepts;
+	int startId = 0, endId = 100;
+	public Ontology(){
+		allConcepts = new ArrayList<Concept>();
+		for(int i = startId; i <= endId; i ++){
+			Concept con = new Concept(i);
+			allConcepts.add(con);
+		}
+		root = allConcepts.get(0);
+	}
+	public void link(int parentId, int childId){
+		Concept parent = allConcepts.get(parentId - startId);
+		Concept child = allConcepts.get(childId - startId);
+		parent.lowerClasses.add(child);
+		child.upperClass = parent;
+	}
 }
