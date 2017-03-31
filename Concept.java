7b54d1eae19c991d4242b6f77d89490d6242a58a
entslscheia <yugu772@yahoo.com>
2017-03-31 11:06:35 +0800

    add descentants to the node and offer some operations on descentants

--- Concept.java
+++ Concept.java
@@ -7,10 +7,11 @@ public class Concept {
 	public int id;
 	public Concept upperClass;
 	public List<Concept> lowerClasses;
-	
+	public List<Concept> descendants;
 	public Concept(int id){
 		this.id = id;
 		upperClass = null;
 		lowerClasses = new ArrayList<Concept>();
+		descendants = new ArrayList<Concept>();
 	}
 }
--- Ontology.java
+++ Ontology.java
@@ -1,7 +1,9 @@
 package name.zanbry.cdc;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.io.*;
 
 import name.zanbry.db.DBAgent;
@@ -9,6 +11,8 @@ import name.zanbry.db.DBAgent;
 public class Ontology {
 	public Concept root;
 	public Concept[] allConcepts = new Concept[458];
+	public int[][] distance = new int[458][458];
+	public int[][] LCS = new int[458][458];
 	int size = 0;
 	public DBAgent dbAgent = DBAgent.getInstance();
 	public Ontology(){
@@ -35,12 +39,103 @@ public class Ontology {
 		parent.lowerClasses.add(child);
 		child.upperClass = parent;
 	}
+	/**
+	 * 
+	 * @param id
+	 * @return the reference of object in allConcepts whose id equals id
+	 */
 	public Concept returnById(int id){
 		for(int i = 0; i < allConcepts.length; i ++){
 			if(allConcepts[i] != null && allConcepts[i].id == id)
 				return allConcepts[i];
 		}
 		return null;
+	}
+	/**
+	 * 
+	 * @param c
+	 * @return the index of concept c in array allConcepts
+	 */
+	public int getIndex(Concept c){
+		for(int i = 0; i < size; i ++){
+			if(allConcepts[i] == c)
+				return i;
+		}
+		return -1;
+	}
+	/**
+	 * 
+	 * @param root
+	 * @return return the depth of tree rooted root
+	 */
+	public int depthByDFS(Concept root){
+		int result = 1;
+		if(root.lowerClasses.size() == 0)
+			return result;
+		else{
+			int max = 0;
+			for(Concept cpt: root.lowerClasses){
+				int d = depthByDFS(cpt);
+				if(d > max)
+					max = d;
+			}
+			result += max;
+		}
+		return result;
+	}
+	/**
+	 * 
+	 * @param root
+	 * @return all the descendants of node root
+	 */
+	@Deprecated
+	public List<Concept> acqDescendant(Concept root){
+		List<Concept> result = new ArrayList<Concept>();
+		if(root.lowerClasses.size() == 0)
+			return root.descendants;
+		result.addAll(root.lowerClasses);
+		for(Concept c: root.lowerClasses){
+			result.addAll(acqDescendant(c));
+		}
+		return result;
+	}
+	/**
+	 * set the data field descendant of all nodes in tree rooted root
+	 * @param root
+	 */
+	@Deprecated
+	public void setDescendant(Concept root){
+		//if(root.lowerClasses.size() == 0)
+		//	return;
+		root.descendants = acqDescendant(root);
+		for(Concept c: root.lowerClasses){
+			setDescendant(c);
+		}
+	}
+	/**
+	 * set the data field descendant of all nodes in tree rooted root
+	 * @param root
+	 */
+	public void Descendant(Concept root){
+		for(Concept c: root.lowerClasses){
+			Descendant(c);
+			root.descendants.add(c);
+			root.descendants.addAll(c.descendants);
+		}
+	}
+	public void calculateByDFS(Concept root){
+		if(root.lowerClasses.size() == 0){
+			int index = getIndex(root);
+			if(index != -1){
+				distance[index][index] = 0;
+				LCS[index][index] = root.id;
+			}
+			else System.out.println("fatal error: index = -1");
+		}
+		else{
+			for(Concept c: root.lowerClasses)
+				calculateByDFS(c);
+		}
 	}
 	public int countByDFS(Concept root){
 		int result = 1;
@@ -111,6 +206,13 @@ public class Ontology {
 	            }
 	        }
 	    }
+		otlg.Descendant(otlg.root);
 		
+		int id = otlg.dbAgent.getId("<http://dbpedia.org/ontology/Work>");
+		for(Concept c: otlg.returnById(id).descendants)
+			System.out.println(c.id);
+		Set<Concept> set = new HashSet<Concept>();
+		set.addAll(otlg.returnById(id).descendants);
+		System.out.println(set.size());
 	}
 }
