fc9f789a89418874feed75bb3d9439a946170661
entslscheia <yugu772@yahoo.com>
2017-04-01 11:09:47 +0800

    some annotation

--- Concept.java
+++ Concept.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Concept {
-	public int id;
-	public Concept upperClass;
+	public int id;// the index of class in database
+	public Concept upperClass;// concept is subclass of upperClass
 	public List<Concept> lowerClasses;
 	public List<Concept> descendants;
 	public Concept(int id){
